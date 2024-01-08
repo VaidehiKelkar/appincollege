235bff4ae1ca8a569042ec1a510de915b9259445
VaidehiKelkar <vaidehikelkar2911@gmail.com>
2024-01-08 10:13:47 +0530

    from college

---
+++ app/src/main/java/com/example/kelkarmishthanna/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.kelkarmishthanna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void register(View view) {
+        startActivity(new Intent(MainActivity.this,Register.class));
+    }
+}
